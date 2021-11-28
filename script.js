35ee2ff1c14329e6777139728251676557e9b620
Anton Ryabtsev <anton_ryabcev@mail.ru>
2021-11-28 16:49:13 +0300

    finished the task with burgers

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ const goods = [
   {},
 ];
 
+class CartItems {
+  constructor(title, price){
+    this.title = title;
+    this.price = price;
+    this.count = 0;
+  }
+  addToCart () {
+    // добавляем товары в корзину с проверкой повторяющихся позиций
+  }
+  removeItems(){
+    //  удаляем товары из корзины
+  }
+  showTotalPrice(){
+    // подсчитываем общую сумму
+  }
+}
+
 class GoodsItem {
   constructor({ title = "Вакантно" , price = 0 }) {
     this.title = title;
-    this.price = price;
+    this.price = price ;
   }
   render() {
     return `
@@ -34,19 +51,80 @@ class GoodsList {
       const goodsItem = new GoodsItem(item);
       return goodsItem.render();
     })
-    debugger
     document.querySelector('.goods-list').innerHTML = _goodsItems.join("");
   }
   getTotalPrice() {
     let totalSumm = 0;
     this.goods.map((item) => {
+      if (item.price == undefined) {
+        item.price = 0;
+        return item.price
+      }
       this.price = item.price
       totalSumm += this.price;
     })
-    console.log(totalSumm);
+    console.log(`Стоимость всех товаров ${totalSumm} у.е.`);
   }
-}
+};
 
 const goodsList = new GoodsList();
 goodsList.render();
 goodsList.getTotalPrice();
+
+
+// гамбургерная
+
+const burgers = [
+  {title: "Большой", price: 100, fat: 40 },
+  {title: "Маленький", price: 50, fat: 20 },
+];
+const garnears = [
+  {title: "Сыр", price: 10, fat: 20 },
+  {title: "Салат", price: 20, fat: 5 },
+  {title: "Картофель", price: 15, fat: 10 },
+]
+const topings = [
+  {title: "Приправа", price: 15, fat: 0 },
+  {title: "Майонез", price: 20, fat: 5 },
+];
+
+class MakeOrder {
+  constructor(title){
+    this.title = title;
+    this.price = 0;
+    this.fat = 0;
+  }
+  calc(item){
+    this.price += item.price;
+    this.fat += item.fat;
+  }
+  chooseBurger(item){
+    // this.burger = item;
+    burgers.map((item) => {
+      if (item.title == this.title){
+        this.calc(item);
+      }
+    })
+    this.addGarnear();
+  }
+  addGarnear(){
+    const garnear = prompt('Каклй гарнир положить?');
+    garnears.map((item) => {
+      if (item.title === garnear) {
+        this.calc(item);
+      }
+    })
+    this.addToping()
+  }
+  addToping(){
+    topings.map((item) => {
+     if ( confirm(`Добавить ${item.title} ?`)) {
+       this.calc(item);
+     }
+    })
+  }
+}
+
+const newOrder = new MakeOrder("Маленький");
+newOrder.chooseBurger();
+console.log(newOrder);
